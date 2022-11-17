9765b140431d15af5da568927ac3b7c65b46f55b
kave haj <kave.haj@snapp.cab>
2022-11-18 01:34:35 +0330

    fix bugs

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -14,19 +13,17 @@ type apiConfiDate struct {
 
 type weatherData struct {
 	Name string `json:"name"`
-	Main        `json:"main"`
+	Main `json:"main"`
 }
 
 type Main struct {
 	Kelvin float64 `json:"temp"`
-	Celcius float64 `json:"celcius"`
 }
-
 
 func loadApiConfig(filename string) (apiConfiDate, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
-	return apiConfiDate{}, err
+		return apiConfiDate{}, err
 	}
 	var c apiConfiDate
 	err = json.Unmarshal(bytes, &c)
@@ -36,6 +33,45 @@ func loadApiConfig(filename string) (apiConfiDate, error) {
 	return c, nil
 }
 
-func main(){
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/weather/", weather)
+	http.ListenAndServe(":8000", nil)
 
+}
+
+func weather(w http.ResponseWriter, r *http.Request) {
+	city := strings.SplitN(r.URL.Path, "/", 3)[2]
+	data, err := query(city)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(data)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from kave!\n"))
+
+}
+
+func query(city string) (weatherData, error) {
+	apiConfig, err := loadApiConfig(".apiConfig")
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+	return d, nil
 }
